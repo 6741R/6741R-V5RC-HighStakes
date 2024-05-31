5ad6a2a7b264829cc5b76b30f1b45b0c829c3581
VoidDroidCD <nindriod7@gmail.com>
2024-05-31 10:07:14 -0400

    C Motor control
    replaced c++ pros motor control with c

--- src/Intake_Control.cpp
+++ src/Intake_Control.cpp
@@ -9,18 +9,18 @@ extern Robot_Config robotDevices;
 // Intakes intake at desired velocity in percent units
 void Intake_Control::Intake(int velocityPercent) {
     // Rotates intake motor forward at full speed
-    robotDevices.intakeMotor.move((1 / velocityPercent) * 127);
+    c::motor_move(13, (1 / velocityPercent) * 127);
 }
 
 // Outtakes intake at desired velocity in percent units
 void Intake_Control::Outtake(int velocityPercent) {
     // Rotates intake motor reversed at full speed
-    robotDevices.intakeMotor.move((1 / velocityPercent) * -127);
+    c::motor_move(13, (1 / velocityPercent) * -127);
 }
 
 void Intake_Control::StopIntake() {
     robotDevices.intakeMotor.set_brake_mode(E_MOTOR_BRAKE_BRAKE);
-    robotDevices.intakeMotor.brake();
+    c::motor_move(13, 0);
 }
 
 // Removes illegal rings from intake
